14d6006d32ff249c7ca1242bfbe1d4631b38b0f0
Jessica Lima <maytajessica73@gmail.com>
2025-09-26 11:16:04 -0300

    Add usuários a tabela

--- main.py
+++ main.py
@@ -16,3 +16,33 @@ CREATE TABLE IF NOT EXISTS alunos (
     curso TEXT
 )
 """)
+
+# nome = input("Digite o nome do aluno: ").lower()
+# idade = int(input("Digite a idade do aluno: "))
+# curso = input("Digite o curso do aluno: ").lower()
+
+# #Inserir um dado na tabela
+# cursor.execute("""
+# INSERT INTO alunos (nome, idade, curso)
+# VALUES (?, ?, ?)
+# """,
+# (nome, idade, curso)
+# )
+
+# #Confirmar as alterações no banco
+# conexao.commit()
+
+#Inserir varios alunos de uma só vez
+alunos = [
+    ("Yago", 28, "Direito"),
+    ("jessica", 24, "Computação"),
+    ("Breno",52, "Computação"),
+]
+#executemany permite inserir múltiplas linhas e uma vez só
+cursor.executemany("""
+INSERT INTO alunos (nome, idade, curso)
+VALUES (?, ?, ?)
+""",
+(alunos)
+)
+conexao.commit()
